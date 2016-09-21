1dfe6002efe4c3b334eea9e1a1b143f750e27fd3
Falish <durejafalish@gmail.com>
2016-09-21 11:01:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('hey there! this is article two...');
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
