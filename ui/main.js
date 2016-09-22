056b21275ea7d82b0074991da7f0eec065766541
Falish <durejafalish@gmail.com>
2016-09-22 12:14:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
 console.log('Loaded!');
 //making a element of html move
 var element = document.getElementById('img');
-element.onclick = function(){
-    element.style.marginLeft = '20px';
+element.onhover = function(){
+    element.style.marginTop = '20px';
 };
 var element1 = document.getElementById('img1');
 element1.onclick = function(){
-    element1.style.marginLeft = '20px';
+    element1.style.marginTop = '20px';
 };
