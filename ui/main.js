f8d2f3e95e55a06479290e5f2eeec525a9bbf735
Falish <durejafalish@gmail.com>
2016-09-22 12:22:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,21 @@
 console.log('Loaded!');
 //making a element of html move
 var element = document.getElementById('img');
-element.onhover = function(){
-    element.style.marginTop = '20px';
+var margin = 2;
+function movedown(){
+    element.style.marginTop = margin + 'px';
+}
+function moveup(){
+    element.style.marginBottom = margin + 'px';
+}
+element.onclick = function(){
+    for(var i=0;i<7;i++){
+    var interval = setInterval(movedown,50);
+    }
+    for(var j=0;j<7;j++){
+        var interval1 = setInterval(moveup,50);
+    }
+    
 };
 var element1 = document.getElementById('img1');
 element1.onclick = function(){
