6b6cc2e89379ad063fd8403fc3351551235eac0a
Falish <durejafalish@gmail.com>
2016-09-22 12:26:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,22 @@
 console.log('Loaded!');
 //making a element of html move
 var element = document.getElementById('img');
-var margin = 20;
+var margin = 0;
 function movedown(){
+    margin = margin + 5;
     element.style.marginTop = margin + 'px';
 }
 function moveup(){
+    margin = margin + 5;
     element.style.marginBottom = margin + 'px';
 }
 element.onclick = function(){
-    
+    for(var i=0;i<7;i++){
     var interval = setInterval(movedown,50);
-    
-    
+    }
+    for(var j=0;j<7;j++){
+        var interval1 = setInterval(moveup,50);
+    }
     
 };
 var element1 = document.getElementById('img1');
