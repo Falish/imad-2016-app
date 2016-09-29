fbfcd1ec05d2f38e1dfa4a8193899cd6bb58fa95
Falish <durejafalish@gmail.com>
2016-09-29 23:17:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,34 @@ button.onclick = function(){
     request.open('GET','http://falish.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+//for list
+var button = document.getElementById('submit_btn');
+button.onclick = function(){
+    //create request
+    var request = new XMLHttpRequest();
+    
+    //capture response
+    request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          //take action
+          if(request.status === 200){
+              var names = request.responseText;
+              names = JSON.parse(names);
+              var list = '';
+              for(var i=0;i<names.length;i++){
+                  list = '<li>' + names[i] + '</li>';
+              }
+              var ul = document.getElementById('list');
+              ul.innerHTML = list.toString();
+              
+          }
+      }  
+    };
+    
+    //make request
+    var name = document.getElementById('name');
+    name = nameInput.value;
+    request.open('GET','http://falish.imad.hasura-app.io/submit'+name,true);
+    request.send(null);
+};
