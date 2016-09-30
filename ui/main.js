9124a079956b53b3d84598e70e4cc6d360b5a0e3
Falish <durejafalish@gmail.com>
2016-09-30 11:49:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,7 +35,7 @@ button1.onclick = function(){
               names = JSON.parse(names);
               var list = '';
               for(var i=0;i<names.length;i++){
-                  list = '<li>' + names[i] + '</li>';
+                  list += '<li>' + names[i] + '</li>';
               }
               var ul = document.getElementById('list');
               ul.innerHTML = list.toString();
