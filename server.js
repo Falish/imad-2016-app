bc4fe2e77a81e02a7a0d3594eb088b08abafcff5
Falish <durejafalish@gmail.com>
2016-10-12 13:11:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,8 +44,8 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/Angles.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Angles.jpg'));
+app.get('/ui/home.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'home.png'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
