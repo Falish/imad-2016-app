2303f5339c1247189a494b9893ed733ebe14f476
Falish <durejafalish@gmail.com>
2016-10-22 20:12:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -46,6 +46,15 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
+app.get('/ui/profile/css/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/profile/css', 'style.css'));
+});
+
+app.get('/ui/profile/css/reset.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/profile/css', 'reset.css'));
+});
+
 app.get('/ui/home.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'home.png'));
 });
