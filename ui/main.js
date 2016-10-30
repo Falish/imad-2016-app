9c4b671bd5851e6057cb30f8fc8932d465342138
Falish <durejafalish@gmail.com>
2016-10-30 12:19:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,19 @@ button.onclick = function(){
     request.send(null);
 };
 
+$(document).ready(function(){
+	$('li.fields').filter(':nth-child(n+4)').addClass('hide');
+	
+	$('ul').on('click','li.title',function(){
+		$(this).next().slideDown(200).siblings('li.fields').slideUp(200);
+	});
+	
+	$connect = mysqli_connect("localhost:5432","falish","db-falish-22325","falish");
+	
+	if(mysqli_connect_errno($connect)){
+	    echo ("Failed to cinnect to mysql.").mysqli_connect_error();
+	}
+});
 //for list
 var button1 = document.getElementById('submit_btn');
 button1.onclick = function(){
