b8e8fc6c2fd7baaa8b440b0ce4f731033f4a3dc9
Falish <durejafalish@gmail.com>
2016-10-30 12:18:19 +0530

    [imad-console] Updates ui/profile/js/animation.js

--- ui/profile/js/animation.js
+++ ui/profile/js/animation.js
@@ -4,4 +4,10 @@ $(document).ready(function(){
 	$('ul').on('click','li.title',function(){
 		$(this).next().slideDown(200).siblings('li.fields').slideUp(200);
 	});
+	
+	$connect = mysqli_connect("localhost:5432","falish","db-falish-22325","falish");
+	
+	if(mysqli_connect_errno($connect)){
+	    echo ("Failed to cinnect to mysql.").mysqli_connect_error();
+	}
 });
